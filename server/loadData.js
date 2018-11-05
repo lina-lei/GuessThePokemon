b86418348905643de065426e22a4f764b09e09a7
Lina Lei <leilsaru@gmail.com>
2018-11-04 21:13:53 -0500

    Update localhost db to mlab

--- server/loadData.js
+++ server/loadData.js
@@ -1,14 +1,13 @@
 // used to load all pokemon data into database (one time operation)
-let pokemonData = require('./data');
-let Pokemon = require('./Pokemon');
-let db = require('./db/index');
+const pokemonData = require('./data');
+const Pokemon = require('./Pokemon');
+const db = require('./db/index');
 
-let insertPokemon = () => {
+const insertPokemon = () => {
   Pokemon.insertMany(pokemonData, (err) => {
-    if(err) console.log('loadData err: ', err);
+    if (err) console.log('loadData err: ', err);
     else console.log('loadData successful');
   });
-  
 };
 
 insertPokemon();
